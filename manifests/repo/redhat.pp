bf8d7fc5cf3ab7cadbb2057c464e492c859a4f91
Ash Crosby <tcrosby@gmail.com>
2020-05-21 15:44:16 +0100

    Better repo var selection

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -10,23 +10,45 @@ class php::repo::redhat (
   $yum_repo = 'remi_php71',
 ) {
 
-  $releasever = $facts['os']['name'] ? {
-    /(?i:Amazon)/ => '6',
-    default       => '$releasever',  # Yum var
+  case $facts['os']['name'] {
+    /(?i:Amazon)/: {
+      $releasever = '6'
+      $distro = 'enterprise'
+      $keyname = 'RPM-GPG-KEY-remi'
+    }
+    'Fedora': {
+      $releasever = '$releasever'
+      $distro = 'fedora'
+      $keyname = $facts['os']['release']['major'] ? {
+        '32'      => 'RPM-GPG-KEY-remi2020',
+        /(30|31)/ => 'RPM-GPG-KEY-remi2019',
+        /(28|29)/ => 'RPM-GPG-KEY-remi2018',
+        /(26|27)/ => 'RPM-GPG-KEY-remi2017',
+        default   => 'RPM-GPG-KEY-remi'
+      }
+    }
+    default: {
+      $releasever = '$releasever'
+      $distro = 'enterprise'
+      $keyname = $facts['os']['release']['major'] ? {
+        '8'     => 'RPM-GPG-KEY-remi2018',
+        default => 'RPM-GPG-KEY-remi'
+      }
+    }
   }
 
   yumrepo { 'remi':
     descr      => 'Remi\'s RPM repository for Enterprise Linux $releasever - $basearch',
-    mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/remi/mirror",
+    mirrorlist => "https://rpms.remirepo.net/${distro}/${releasever}/remi/mirror",
     enabled    => 1,
     gpgcheck   => 1,
-    gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+    gpgkey     => "https://rpms.remirepo.net/${keyname}",
     priority   => 1,
   }
 
   yumrepo { 'remi-php56':
     descr      => 'Remi\'s PHP 5.6 RPM repository for Enterprise Linux $releasever - $basearch',
-    mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/php56/mirror",
+    mirrorlist => "https://rpms.remirepo.net/${distro}/${releasever}/php56/mirror",
     enabled    => 1,
     gpgcheck   => 1,
     gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
